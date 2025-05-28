e1a22e16b64fb13e11835567b9142b0acce1472b
Meshwa-Simform <meshwa.patel@simformsolutions.com>
2025-05-28 12:06:56 +0530

    Revert "pract: pipes assigment to mask phone number"

--- Pipe_Assignment/src/app/pipes/mask-phonenu.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MaskPhonenuPipe } from './mask-phonenu.pipe';
-
-describe('MaskPhonenuPipe', () => {
-  it('create an instance', () => {
-    const pipe = new MaskPhonenuPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
